82d6e7cec8ec3f01a045824844d0f19dd466e29d
divyansh1211 <divyanshmanchanda1211@gmail.com>
2024-08-21 03:23:52 +0530

    Fixed minor bugs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,48 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import { SignUp } from "./pages/signup";
 import { SignIn } from "./pages/signin";
 import { Blog } from "./pages/blog";
 import { BlogWithId } from "./pages/BlogWithId";
 import { Publish } from "./pages/Publish";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem("jwt") !== null) {
+      setIsAuthenticated(true);
+      navigate("/blogs");
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/signup"
-            element={
-              localStorage.getItem("jwt") === null ? <SignUp /> : <Blog />
-            }
-          />
-          <Route
-            path="/signin"
-            element={
-              localStorage.getItem("jwt") === null ? <SignIn /> : <Blog />
-            }
-          />
-          <Route path="/blogs/:id" element={<BlogWithId />} />
-          <Route path="/blogs" element={<Blog />} />
-          <Route
-            path="/"
-            element={
-              localStorage.getItem("jwt") === null ? <SignUp /> : <Blog />
-            }
-          />
-          <Route path="/publish" element={<Publish />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route
+          path="/signup"
+          element={isAuthenticated ? <Blog /> : <SignUp />}
+        />
+        <Route
+          path="/signin"
+          element={isAuthenticated ? <Blog /> : <SignIn />}
+        />
+        <Route
+          path="/blogs/:id"
+          element={isAuthenticated ? <BlogWithId /> : <SignUp />}
+        />
+        <Route
+          path="/blogs"
+          element={isAuthenticated ? <Blog /> : <SignUp />}
+        />
+        <Route path="/" element={isAuthenticated ? <Blog /> : <SignUp />} />
+        <Route
+          path="/publish"
+          element={isAuthenticated ? <Publish /> : <SignUp />}
+        />
+      </Routes>
     </>
   );
 }
