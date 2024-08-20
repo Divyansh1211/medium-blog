7254fa079b5c6d037972979a516543eeeb1c295e
divyansh1211 <divyanshmanchanda1211@gmail.com>
2024-08-21 03:26:42 +0530

    Fixed browser router bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import { SignUp } from "./pages/signup";
 import { SignIn } from "./pages/signin";
